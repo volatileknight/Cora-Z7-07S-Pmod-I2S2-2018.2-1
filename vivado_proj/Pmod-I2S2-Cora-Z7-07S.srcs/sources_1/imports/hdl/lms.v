c4928cc426665577516c0f9600224107d6a9bfb5
Niaolan27 <jniow@andrew.cmu.edu>
2026-01-16 21:25:51 -0500

    updated lms

--- vivado_proj/Pmod-I2S2-Cora-Z7-07S.srcs/sources_1/imports/hdl/lms.v
+++ vivado_proj/Pmod-I2S2-Cora-Z7-07S.srcs/sources_1/imports/hdl/lms.v
@@ -10,11 +10,27 @@ module LMS #(
     output reg  signed [COEFF_WIDTH*NUM_TAPS-1:0] wn_u       // flattened coefficients
 );
 
-    parameter signed [23:0] gamma = 24'sh000CCD; // ≈ 0.05
+    parameter signed [23:0] gamma = 24'sh000CCD; // Q0.23 ~= 0.00039
 
     // Internal registers (flattened)
     reg signed [COEFF_WIDTH-1:0] wn [0:NUM_TAPS-1];
     integer i;
+
+    function automatic signed [COEFF_WIDTH-1:0] sat_coeff;
+        input signed [COEFF_WIDTH:0] x;
+        reg signed [COEFF_WIDTH-1:0] maxv;
+        reg signed [COEFF_WIDTH-1:0] minv;
+    begin
+        maxv = {1'b0, {(COEFF_WIDTH-1){1'b1}}};
+        minv = {1'b1, {(COEFF_WIDTH-1){1'b0}}};
+        if (x > maxv)
+            sat_coeff = maxv;
+        else if (x < minv)
+            sat_coeff = minv;
+        else
+            sat_coeff = x[COEFF_WIDTH-1:0];
+    end
+    endfunction
 
     // LMS update
     always @(posedge clk or negedge reset_n) begin
@@ -29,10 +45,22 @@ module LMS #(
                 // Note: [msb +: width] works in Verilog-2001
                 // Flattened slice: bits i*INPUT_WIDTH to (i+1)*INPUT_WIDTH-1
                 // Extend to wider bit width for multiply
-                wn[i] <= wn[i] + ((gamma * (err * buff_array[i*INPUT_WIDTH +: INPUT_WIDTH])) >>> INPUT_WIDTH);
-                
-                // Update flattened output
-                wn_u[i*COEFF_WIDTH +: COEFF_WIDTH] <= wn[i];
+                reg signed [2*INPUT_WIDTH-1:0] err_x;
+                reg signed [2*INPUT_WIDTH+COEFF_WIDTH-1:0] mult;
+                reg signed [2*INPUT_WIDTH+COEFF_WIDTH-1:0] mult_round;
+                reg signed [COEFF_WIDTH-1:0] delta;
+                reg signed [COEFF_WIDTH:0] wn_sum;
+
+                err_x = err * buff_array[i*INPUT_WIDTH +: INPUT_WIDTH];
+                mult = gamma * err_x;
+                mult_round = mult + (mult[2*INPUT_WIDTH+COEFF_WIDTH-1] ? -(1'sb1 <<< (INPUT_WIDTH-1))
+                                                                      :  (1'sb1 <<< (INPUT_WIDTH-1)));
+                delta = mult_round >>> INPUT_WIDTH;
+                wn_sum = wn[i] + delta;
+                wn[i] <= sat_coeff(wn_sum);
+
+                // Update flattened output with the new coefficient
+                wn_u[i*COEFF_WIDTH +: COEFF_WIDTH] <= sat_coeff(wn_sum);
             end
         end
     end
